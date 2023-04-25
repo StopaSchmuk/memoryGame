dfc69d977d6ca49c1c74e7448aae24d1a3071dfd
stopaschmuck <pran1337@bk.ru>
2023-04-25 22:27:25 +0600

    create start actions ver.1

--- interaction.js
+++ interaction.js
@@ -1,35 +1,32 @@
-let users = ['Vanya', 'Gleb', 'Andrey', 'Kirill', 'Ishtvan', 'Ishtvan',];
-let usersTwo = ['Zhenya','Vanya','Gleb', 'Andrey', 'Kirill', 'Ishtvan', 'Ishtvan',];
-users.push('Olya');
-console.log(users);
-users.forEach(function (item, index, array) {
-  
-  if (item === 'Ishtvan') {
-    return users[index] = 'Petya';
-  };
+let startButtonElem = document.createElement('button');
+startButtonElem.className = "startBut";
+startButtonElem.innerText = "Start!";
+startButtonElem.id = "startBut";
+startInterface.append(startButtonElem);
+
+const startBut = document.getElementById('startBut');
+startBut.addEventListener('click', function () {
+  setTimeout(deleteStartBut, 700)
 });
-console.log(users);
-function deleteElem(array) {
-  let newUsers = array.slice();
-  let result = newUsers.splice(0, 1);
-  return result;
-};
+//==========================================================================================//
 
-function addNewElems(array, newElem1, newElem2) {
-  
-  array.splice(0, 0, `${newElem1}`, `${newElem2}`);
-  return array;
-};
-function arrToStr(array) {
-  array.join(",  ");
-  return array;
+let gameAreaDiv = document.createElement('div');
+gameAreaDiv.className = "gameAreaDiv";
+gameAreaDiv.id = "gameAreaDiv";
+let gameBlock1 = document.createElement('button');
+gameBlock1.className = "gameBlock1";
+let gameBlock2 = document.createElement('button');
+gameBlock2.className = "gameBlock2";
+let gameBlock3 = document.createElement('button');
+gameBlock3.className = "gameBlock3";
+function addGameElems() {
+  startInterface.append(gameAreaDiv);
+  gameAreaDiv.append(gameBlock1);
+  gameAreaDiv.append(gameBlock2);//глючит
+  gameAreaDiv.append(gameBlock3);
+}
+function deleteStartBut() {//удаляет и СТАВИТ НУЖНЫЕ ОБЪЕКТЫ ДЛЯ ИГРЫ
+  startButtonElem.remove()
+  setTimeout(addGameElems, 500);
 
 };
-console.log(deleteElem(usersTwo));
-console.log(usersTwo);
-console.log(addNewElems(usersTwo, "Egor", "Vlad"));
-console.log(arrToStr(usersTwo));
-let elemDiv = document.createElement('p');
-elemDiv.innerText = arrToStr(addNewElems(usersTwo, "Egor", "Vlad"));
-document.body.append(elemDiv);
-
